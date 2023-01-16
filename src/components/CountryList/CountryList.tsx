eff9274547cdd61e2df97a6820566480d43a2e78
Eros Nicolau <eros.nicolau@gmail.com>
2023-01-16 16:56:55 +0200

    Sketching initial file structure

---
+++ src/components/CountryList/CountryList.tsx
@@ -0,0 +1,10 @@
+import Country from '../Country/Country';
+import { useState } from 'react';
+
+export const CountryList: React.FC = () => {
+  return (
+    <div>
+      <Country />
+    </div>
+  );
+}
